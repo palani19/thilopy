0e231a685d91dcfea1e149752314b5747652d6ab
palani19 <43022965+palani19@users.noreply.github.com>
2018-09-20 09:01:50 +0530

    Add files via upload

---
+++ 18.py
@@ -0,0 +1,14 @@
+x=int(input("Enter your marks"))
+#to print grade for marks
+if(x>=90):
+	print('A+',"is your grade")
+elif(x>=80):
+	print('A',"is your grade")
+elif(x>=70):
+	print('B',"is your grade")	
+elif(x>=60):
+	print('B+',"is your grade")
+elif(x>=40):
+	print('C',"is your grade")
+else:
+	print("Fail")
