e4f1772e3f6931869084f8acf10b703cfa7f8a96
palani19 <43022965+palani19@users.noreply.github.com>
2018-11-22 09:35:49 +0530

    Add files via upload

---
+++ new1.py
@@ -0,0 +1,4 @@
+f=open("dl2.txt","w")
+f.write("file operations")
+f=open("dl2.txt","r")
+print(f.read())
---
+++ new2.py
@@ -0,0 +1,4 @@
+f=open("dl2.txt","a")
+f.write("file operations")
+f=open("dl2.txt","r")
+print(f.read())
